759d00940f53c995c70af4860235900bd96aa411
Mark Modak <mmodak@mmodak.local>
2019-01-31 14:51:04 -0700

    First solution for Ruby word_count

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -1,4 +1,3 @@
-require 'pry'
 class Phrase
 
   attr_reader :phrase
@@ -7,28 +6,16 @@ class Phrase
   end
 
   def word_count
-     words = @phrase.split(/\b(?!\s)/)
-     frequencies = Hash.new(0)
-     words.each do |word|
-       word.each { |w| word.split("").each_ { |l| l.delete if l !}} (/[a-zA-Z]/) === word[0]
-
-       frequencies[word] += 1
-       end
-     end
-     frequencies
-   end
-
+    words = @phrase.scan(/(?!'.*')\b[\w']+\b/)
+    frequencies = Hash.new(0)
+    words.each do |word|
+      word.downcase!
+      if frequencies[word]
+        frequencies[word] += 1
+      else
+        frequencies[word] = 1
+      end
+    end
+    frequencies
+  end
 end
-
-# def word_count
-#   hash = {}
-#   array = phrase.split(/\b(?!\s)/)
-#   array.each { |w| w.strip! }
-#   array.each { |w| w.downcase! }
-#   array.each do |w|
-#     n = array.count(w)
-#     hash[w] = n if w.count("a-z", 0-1) != 0
-#     array - [w]
-#   end
-#   hash
-# end
