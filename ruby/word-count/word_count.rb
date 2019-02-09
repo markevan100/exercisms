ad6c02c85c88309cd2a78c1f5d0c1c709878a62a
Mark Modak <mmodak@mmodak.local>
2019-02-08 21:18:17 -0700

    More refactor for word-count

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -1,20 +1,17 @@
 class Phrase
 
-  attr_reader :phrase
   def initialize(phrase)
     @phrase = phrase
   end
 
   def word_count
-    words.each_with_object({}) do |word, hash|
-      word.downcase!
-      hash.default = 0
-      hash[word] += 1
-    end
+    words.each_with_object(Hash.new(0)) { |word, hash| hash[word] += 1 }
   end
 
+  private
+  attr_reader :phrase
+  
   def words
-    phrase.scan(/\b[\w']+\b/)
+    phrase.downcase.scan(/\b[\w']+\b/)
   end
-
 end
