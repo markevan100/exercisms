13cd4b898443a27e521f54042f6465178aa3904b
Mark Modak <mmodak@mmodak.local>
2019-02-12 06:32:17 -0700

    Refactor hamming to use .count and remove counter

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -2,9 +2,7 @@ class Hamming
 
   def self.compute(original, mutation)
     raise ArgumentError if original.length != mutation.length
-    counter = 0
     array = original.split("")
-    array.each_index { |i| counter += 1 if original[i] != mutation[i] }
-    counter
+    array.each.with_index.count { |x, i| x != mutation[i] }
   end
 end
